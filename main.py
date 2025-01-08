0c2e6c9449d9f0456b93b3351be3144fe127779e
Wheeljack1986 <bullonrod.alexadrian@gmail.com>
2025-01-07 20:25:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "CREEPY": "aterrador, siniestro",
+            "XD": "Es una risa"
+            }
+while True:
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("lo siento pero no sabemos de esa palabra :V")
